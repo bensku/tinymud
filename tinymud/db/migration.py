b4138c90db586fcc0c6644bd073bd931cb3b807f
bensku <bmi@iki.fi>
2020-10-29 00:17:17 +0200

    Add table creation/migration tools for game entities (WIP)

---
+++ tinymud/db/migration.py
@@ -0,0 +1,84 @@
+"""High-level data migration utils."""
+
+import json
+from typing import Optional
+from pathlib import Path
+
+from asyncpg import Connection
+
+from tinymud.db.schema import TableSchema, get_create_table
+
+
+class MigrationException(Exception):
+    pass
+
+
+class TableMigrator:
+    def __init__(self, conn: Connection, db_data: Path, prod_mode: bool):
+        self.conn = conn
+        self.migrations = db_data / 'migrations'
+        self.schemas = db_data / 'schemas'
+        self.prod_mode = prod_mode
+
+    async def _get_migration_level(self, table: str) -> Optional[int]:
+        """Gets current migration level of a table.
+
+        Returns current migration level (integer) for existing tables and None
+        for those that have not yet been created."""
+        return await self.conn.fetchval('SELECT level FROM tinymud_migrations WHERE table = $1', table)
+
+    async def _set_migration_level(self, table: str, level: int) -> None:
+        """Sets current migration level of a table."""
+        await self.conn.execute('UPDATE tinymud_migrations SET level = $1 WHERE table = $2', level, table)
+
+    async def _schema_valid_prod(self, table: TableSchema) -> bool:
+        """Checks if schema of given table is valid for production.
+
+        When the schemas stored in db_data/schemas and generated from current
+        Python classes differ, some tables probably need migrations that have
+        not been written yet. A developer must address this before pushing to
+        production (or production-like database).
+        """
+        try:
+            with open(self.schemas / table['name'] + '.json', 'r') as f:
+                disk_schema = json.load(f)
+                return table == disk_schema  # Compare schema content
+        except FileNotFoundError:
+            return False  # No schema found!
+
+    async def _run_script(self, path: Path) -> None:
+        """Loads an SQL script from file and run it."""
+        with open(path, 'r') as f:
+            async with self.conn.transaction():
+                self.conn.executemany(f.read())
+
+    async def _run_migrations(self, table: str, current_level: int) -> int:
+        """Run migrations that have not been applied yet."""
+        sql_dir = self.migrations / table
+        for migration in sorted(sql_dir.iterdir()):
+            level = int(migration.name.split('_')[0])
+            if level > current_level:  # Not yet applied
+                await self._run_script(sql_dir / migration)
+
+        if level != current_level:  # Update migration level if needed
+            await self._set_migration_level(table, level)
+        return level
+
+    async def _create_table(self, table: TableSchema) -> None:
+        """Creates a new table."""
+        await self.conn.execute(get_create_table(table))
+        # Initialize migration level (so that it can be altered in future)
+        await self.conn.execute('INSERT INTO tinymud_migrations (table, level) VALUES ($1, $2)', table, 0)
+
+    async def migrate_table(self, table: TableSchema) -> None:
+        name = table['name']
+        # Do a basic sanity check if we're in production environment
+        if self.prod_mode and not await self._schema_valid_prod(table):
+            raise MigrationException(f"in prod, and table {name} has outdated schema")
+
+        # Migrate or create table
+        current_level = await self._get_migration_level(name)
+        if current_level is None:  # Create new table
+            await self._create_table(table)
+        else:  # Run migrations if needed
+            await self._run_migrations(name, current_level)
---
+++ tinymud/db/schema.py
@@ -0,0 +1,128 @@
+"""Table schema management tools."""
+
+from dataclasses import dataclass
+from typing import Dict, List, Tuple, TypedDict, Union
+
+
+class Column(TypedDict):
+    """A database table column."""
+    name: str
+    db_type: str
+    nullable: bool
+
+
+def create_column(name: str, py_type: type) -> Column:
+    """Creates a database column from Python type."""
+    db_type, nullable = _to_db_type(py_type)
+    return {'name': name, 'db_type': db_type, 'nullable': nullable}
+
+
+def _to_db_type(py_type: type) -> Tuple[str, bool]:
+    """Maps a Python type to database type name."""
+    if py_type == Union:  # Optional[type] aliases to Union[type, None]
+        # Mypy has incomplete types here
+        args: List[type] = py_type.__args__  # type: ignore
+        # args contains classes, not instances of them
+        if len(args) == 2 and args[1] == type(None):  # noqa: E721
+            return _to_db_type(args[0])[0], True  # Nullable type
+        else:
+            raise TypeError(f"unsupported union type {py_type}")
+    elif py_type == bool:
+        return 'boolean', False
+    elif py_type == int:
+        return 'integer', False
+    elif py_type == float:
+        return 'double precision', False
+    elif py_type == str:
+        return 'text', False
+    else:
+        raise TypeError(f"unsupported type {py_type}")
+
+
+class TableSchema(TypedDict):
+    """A database table schema."""
+    name: str
+    columns: List[Column]
+
+
+def new_table_schema(name: str, fields: Dict[str, type]) -> TableSchema:
+    """Creates a new table schema from class fields."""
+    columns: List[Column] = []
+    # Id (primary key) always first
+    columns.append(create_column('id', fields['id']))
+    del fields['id']
+
+    # Rest of columns in alphabetical order
+    for name in sorted(fields.keys()):
+        columns.append(create_column(name, fields[name]))
+    return {'name': name, 'columns': columns}
+
+
+def get_create_table(table: TableSchema) -> str:
+    """Gets CREATE TABLE statement for given table."""
+    # Column creation rules
+    col_rows = []
+    for column in table['columns']:
+        row = f'{column["name"]} {column["db_type"]}'
+        if not column['nullable']:
+            row += ' NOT NULL'
+        col_rows.append(row)
+
+    cols_str = ',\n'.join(col_rows)
+    return f'CREATE TABLE {table["name"]} (\n{cols_str}\n)'
+
+
+@dataclass
+class AlterRequest:
+    """A request to alter table."""
+    description: str
+    sql: List[str]
+    input_needed: Dict[str, str] = {}
+
+
+def update_table_schema(old_schema: TableSchema, fields: Dict[str, type]) -> Tuple[TableSchema, List[AlterRequest]]:
+    """Creates an updated table schema based on current class fields.
+
+    Columns in old schema that do not have matching fields are removed.
+    New fields are given columns after other columns in alphabetical order.
+
+    A new schema and a list of table alterations needed to implement it in an
+    existing database are returned.
+    """
+    table_name = old_schema['name']
+    old_columns = old_schema['columns']  # Won't modify this
+    new_columns = old_columns.copy()  # Will remove and add columns here
+    alter_requests: List[AlterRequest] = []  # Alter requests to show to users
+
+    # Remove columns that no longer have fields
+    field_names = fields.keys()
+    old_names = []
+    for column in old_columns:
+        name = column['name']
+        if name not in field_names:
+            new_columns.remove(column)
+            alter_requests.append(AlterRequest(f"drop column {name}",
+                [f'ALTER TABLE {table_name} DROP COLUMN {name}']))
+        else:
+            old_names.append(name)
+
+    # Append columns for new fields at end
+    for name in sorted(fields.keys()):
+        if name not in old_names:
+            column = create_column(name, fields[name])
+            new_columns.append(column)
+
+            # SQL to add a new column for non-null columns is not one-liner
+            sql = [f'ALTER TABLE {table_name} ADD COLUMN {name} {column["db_type"]}']
+            if column['nullable']:
+                alter_requests.append(AlterRequest(f"add nullable column {name}", sql))
+            else:
+                # These queries are written to SQL scripts, thus we can't use prepared statements
+                # (not that there is any need, only trusted admins can use them)
+                sql.append(f'UPDATE {table_name} SET {name} = $existing_value$')
+                sql.append(f'ALTER TABLE {table_name} ALTER COLUMN {name} SET NOT NULL')
+                alter_requests.append(AlterRequest(f"add non-null column {name}",
+                    sql, {'$existing_value$': "value needed for existing rows"}))
+
+    return {'name': table_name, 'columns': new_columns}, alter_requests
+
