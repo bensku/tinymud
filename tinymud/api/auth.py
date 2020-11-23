581c5253bc335d4586b9c579665716e326ffec3a
bensku <bmi@iki.fi>
2020-11-24 00:14:29 +0200

    Add auth token expiration and renewal support

--- tinymud/api/auth.py
+++ tinymud/api/auth.py
@@ -1,6 +1,7 @@
 """REST API for authentication etc."""
 
 import secrets
+import time
 from typing import TypedDict
 
 from aiohttp.web import Application, Request, Response, RouteTableDef
@@ -18,6 +19,7 @@ _jwt_secret: bytes = secrets.token_bytes(64)
 class AuthToken(TypedDict):
     """Authentication token, passed around as JWT."""
     user_id: int
+    exp: int
 
 
 class LoginRequest(BaseModel):
@@ -25,17 +27,34 @@ class LoginRequest(BaseModel):
     password: str
 
 
+def create_token(user_id: int) -> AuthToken:
+    """Creates a new authentication token.
+
+    The token is valid for a while, and its validity starts now.
+    """
+    valid_until = int(time.time()) + 3600  # 1 hour - TODO configurable token lifetime
+    return {'user_id': user_id, 'exp': valid_until}
+
+
 @routes.post('/login')
 async def login(request: Request) -> Response:
     details = LoginRequest(**await request.json())
     # Will throw if credentials are not valid
     user = await validate_credentials(details.name, details.password)
-    token: AuthToken = {'user_id': user.id}
+    token = create_token(user.id)
     return Response(body=jwt.encode(token, _jwt_secret, 'HS256'))
 
 
+@routes.post('/renew')
+async def renew(request: Request) -> Response:
+    # Won't renew unless it is actually valid
+    old_token = validate_token(await request.json())
+    new_token = create_token(old_token['user_id'])  # Same user, new expiration time
+    return Response(body=jwt.encode(new_token, _jwt_secret, 'HS256'))
+
+
 def validate_token(token: str) -> AuthToken:
-    return jwt.decode(token, _jwt_secret)  # type: ignore
+    return jwt.decode(token, _jwt_secret, algorithms=['HS256'])  # type: ignore
 
 
 auth_app.add_routes(routes)
